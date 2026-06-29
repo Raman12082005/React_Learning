8f2ea74f37d28fcb35856018ae61f720839be673
Raman12082005 <rdeep1208y@gmail.com>
2026-06-29 10:58:10 +0530

    adding a folder of project

---
+++ second project/src/components/Card.jsx
@@ -0,0 +1,15 @@
+// function Card(){
+//     return "hi I am Card"
+// }
+// export default Card
+
+const Card = () =>{
+    return(
+        <div className="card">
+            <h1>Manav</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ipsa totam atque molestias maxime? Provident cumque voluptates quae sit nulla aperiam consequuntur rem, temporibus aut corporis similique voluptatum vel. Magni!
+            Accusantium animi facilis odit quidem modi perferendis corporis dignissimos recusandae obcaecati totam est, optio iure quos fugiat, temporibus necessitatibus porro quaerat dolor nihil, hic tempore doloribus! Culpa explicabo ipsa distinctio.</p>
+        </div>
+    )
+}
+export default Card
